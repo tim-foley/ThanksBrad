7ae5fafe37abeec2e51e64b720f91ba3cc1eb60b
Ethan <edsalisb90@gmail.com>
2019-10-05 15:23:34 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,21 +4,22 @@ const app = express();
 const brad = 'UJT4QPQ90'
 const request = require('request');
 
-function thankBrad(token, event){
+function thankBrad(token, event) {
     if (!typeof event !== 'object') return;
 
     const THANKS_MESSAGES = [
         'Thanks!'
     ]
-    request.post('https://slack.com/api.chat.postMessage', {
-        body: {
-            token: token,
-            channel: event.channel,
-            text: `${THANKS_MESSAGES[Math.floor(Math.random() * THANKS_MESSAGES.length)]} <${event.user}> !`,
-            thread_ts: event.thread_ts || undefined,
-            username: 'Brad Boyes'
-    }}, (err, result) => {
-        if (err){
+    const body = {
+        token: token,
+        channel: event.channel,
+        text: `${THANKS_MESSAGES[Math.floor(Math.random() * THANKS_MESSAGES.length)]} <${event.user}> !`,
+        thread_ts: event.thread_ts || undefined,
+        username: 'Brad Boyes'
+    }
+    console.log('body', body)
+    request.post('https://slack.com/api.chat.postMessage', { body }, (err, result) => {
+        if (err) {
             console.error('AN ERROR OCCURRED', err);
             return;
         }
@@ -28,14 +29,14 @@ function thankBrad(token, event){
 
 
 app.use(bodyParser());
-app.post('/', function(req, res){
+app.post('/', function (req, res) {
     const body = req.body;
-    if (!body){
+    if (!body) {
         res.status(200).send({});
         return;
     }
-    if (body && body.type === 'url_verification'){
-        res.status(200).send({challenge: body.challenge});
+    if (body && body.type === 'url_verification') {
+        res.status(200).send({ challenge: body.challenge });
         return;
     }
     console.log('my bot payload', body);
@@ -43,12 +44,12 @@ app.post('/', function(req, res){
     console.log('text type', typeof event.text);
     console.log('isitbrad', event.text.indexOf(brad) > -1)
 
-    if (typeof event.text === 'string' && event.text.indexOf(brad) > -1){
+    if (typeof event.text === 'string' && event.text.indexOf(brad) > -1) {
         thankBrad(body.token, event);
         res.status(200).send({});
         return;
     }
-    
+
     res.status(200).send('OK')
 })
 
