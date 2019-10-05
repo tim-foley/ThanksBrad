9035418123802c6ad28bef45a7375a0d735cb2c9
Ethan <edsalisb90@gmail.com>
2019-10-05 15:27:43 -0700

    Update index.js

--- index.js
+++ index.js
@@ -53,8 +53,10 @@ app.post('/', function (req, res) {
         });
         
     }
-
-    res.status(200).send('OK')
+    else{
+        res.status(200).send('OK')
+    }
+    
 })
 
 app.listen(process.env.PORT || 4747, () => console.log('Server is live and good'));
