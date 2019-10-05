44353e21833a9c2313ee5ca03384f0956e800a67
Ethan <edsalisb90@gmail.com>
2019-10-05 14:51:18 -0700

    oops

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(bodyParser());
 app.post('/', function(req, res){
     const body = req.body;
     if (body && body.type === 'url_verification'){
-        res.status.send(200).send({challenge: body.challenge});
+        res.status(200).send({challenge: body.challenge});
         return;
     }
     console.log('my bot payload', body);
