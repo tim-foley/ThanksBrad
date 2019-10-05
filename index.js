afd79235efa07a6c8868cae88d8b24276fb3441c
Ethan <edsalisb90@gmail.com>
2019-10-05 14:49:03 -0700

    Verifying bot with slack

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.use(bodyParser());
 
 app.post('/', function(req, res){
     const body = req.body;
+    if (body && body.type === 'url_verification'){
+        res.status.send(200).send({challenge: body.challenge});
+        return;
+    }
     console.log('my bot payload', body);
     res.status(200).send('OK')
 })
