38daa69e7b6b03424f99ab007be29a39e999c0c4
Ethan <edsalisb90@gmail.com>
2019-10-05 15:11:01 -0700

    Testing brad bot

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-app.use(bodyParser());
+const brad = '@UJT4QPQ90'
+const request = require('request');
 
+function thankBrad(event){
+    if (!typeof event !== 'object') return;
+
+    const THANKS_MESSAGES = [
+        'Thanks!'
+    ]
+    request.post('https://slack.com/api.chat.postMessage', {
+        body: {
+            token: event.token,
+            channel: event.channel,
+            text: `${THANKS_MESSAGES[Math.floor(Math.random() * THANKS_MESSAGES.length)]} <${event.user}> !`,
+            thread_ts: event.thread_ts || undefined,
+            username: 'Brad Boyes'
+    }}, (err, result) => {
+        if (err){
+            console.error('AN ERROR OCCURRED', err);
+            return;
+        }
+
+    })
+}
+
+
+app.use(bodyParser());
 app.post('/', function(req, res){
     const body = req.body;
+    if (!body){
+        res.status(200).send({});
+        return;
+    }
     if (body && body.type === 'url_verification'){
         res.status(200).send({challenge: body.challenge});
         return;
     }
     console.log('my bot payload', body);
+    const event = body ? body.event : null;
+    if (typeof event.text === 'string' && event.text.indexOf(brad) > -1){
+        thankBrad(event);
+        res.status(200).send({});
+        return;
+    }
+    
     res.status(200).send('OK')
 })
 
